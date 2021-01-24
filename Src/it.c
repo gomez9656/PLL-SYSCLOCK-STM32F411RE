00e077f321c9626c0a5b9958f0025114aa8574ec
juanpg9656@gmail.com <juanpg9656@gmail.com>
2021-01-24 09:55:48 -0500

    Copy project from USART2_example and refactor code so can use it to the HSE_SYSCLK_8MHZ

--- Src/it.c
+++ Src/it.c
@@ -5,19 +5,12 @@
  *      Author: PC
  */
 
-#include "stm32f4xx.h"
-UART_HandleTypeDef huart2; //Handle of UART 2
+#include "main.h"
 
 void SysTick_Handler(void){
 
 	HAL_IncTick();
 	HAL_SYSTICK_IRQHandler();
-}
-
-void USART2_IRQHandler(){
-
-	HAL_UART_IRQHandler(&huart2);
-
 }
 
 
--- Src/main.c
+++ Src/main.c
@@ -16,48 +16,25 @@
 #define TRUE	1
 #define FALSE	0
 
-void SystemCoreClockConfig();
 void UART2_Init();
 void Error_handler();
-uint8_t conver_to_capital(uint8_t data);
 
 UART_HandleTypeDef huart2; //Handle of UART 2
 
-uint8_t data_buffer[100];
-uint8_t received_data;
-uint32_t count = 0;
-uint8_t reception_complete = FALSE;
-
-char *user_data = "The application is up!\r\n";
 
 int main(){
 
 	/* Basic initialization */
 	HAL_Init();
-	SystemCoreClockConfig();
 
 	UART2_Init();
 
-	uint16_t len_of_data = strlen(user_data);
-	HAL_UART_Transmit(&huart2, (uint8_t*)user_data, len_of_data, HAL_MAX_DELAY);
-
-	while(reception_complete != TRUE){
-
-		HAL_UART_Receive_IT(&huart2, &received_data, 1);
-	}
 
 	while(1);
 
 	return 0;
 }
 
-/*
- * You can use it when you need an specific clock
- * By default will be internal clock
- */
-void SystemCoreClockConfig(){
-
-}
 
 /*
  * Function to initialize the UART2 peripheral
@@ -83,27 +60,6 @@ void UART2_Init(){
 	}
 }
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
-
-	if(received_data == '\r'){
-
-		reception_complete = TRUE; //To stop calling HAL_UART_Receive_IT()
-		data_buffer[count++] = '\r';
-		HAL_UART_Transmit(huart, data_buffer, count, HAL_MAX_DELAY);
-	}else{
-
-		data_buffer[count++] = received_data;
-	}
-}
-
-uint8_t conver_to_capital(uint8_t data){
-
-	if( data >= 'a' && data <= 'z'){
-		data = data - ('a' - 'A');
-	}
-
-	return data;
-}
 
 void Error_handler(void){
 	while(1);
