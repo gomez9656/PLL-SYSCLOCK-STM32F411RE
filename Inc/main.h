0fbbd96915145ef925ef6b346283f33c73b979ac
juanpg9656@gmail.com <juanpg9656@gmail.com>
2021-01-24 13:24:58 -0500

    Added code to use the PLL engine to 50, 84 and 120MHz. Compilled

--- Inc/main.h
+++ Inc/main.h
@@ -10,4 +10,8 @@
 
 #include "stm32f4xx.h"
 
+#define SYS_CLOCK_FREQ_50MHZ	50
+#define SYS_CLOCK_FREQ_84MHZ	84
+#define SYS_CLOCK_FREQ_120MHZ	120
+
 #endif /* MAIN_H_ */
--- Src/main.c
+++ Src/main.c
@@ -18,6 +18,7 @@
 #define FALSE	0
 
 void UART2_Init();
+void SystemClockConfig(uint8_t clock_freq);
 void Error_handler();
 
 UART_HandleTypeDef huart2; //Handle of UART 2
@@ -25,77 +26,63 @@ UART_HandleTypeDef huart2; //Handle of UART 2
 
 int main(){
 
-
-	RCC_OscInitTypeDef osc_init;
-	RCC_ClkInitTypeDef clk_init;
-	char msg[100];
-
 	/* Basic initialization */
 	HAL_Init();
 
 	UART2_Init();
 
-	/*
-	 * Oscillator initialization. We are not actually using HSE because it does not exist
-	 * But, we use the bypass mode and is configured in this way. This initialization
-	 * starts the HSE, but that doesn't mean the system clock will use it
-	 */
-	memset(&osc_init, 0, sizeof(osc_init)); //Maybe there is garbage values, so memset to 0
-	osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	osc_init.HSEState = RCC_HSE_BYPASS;
-	if(HAL_RCC_OscConfig(&osc_init) != HAL_OK){
+	SystemClockConfig(50);
 
-		Error_handler();
-	}
-
-	/*
-	 * This part configures the clock to be used as the system clock and
-	 * set different preescalers
-	 */
-	clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
-						 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-	clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
-	clk_init.AHBCLKDivider = RCC_SYSCLK_DIV2;
-	clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
-	clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
-	//This API changes from HSI to HSE
-	if(HAL_RCC_ClockConfig(&clk_init, FLASH_ACR_LATENCY_0WS) != HAL_OK){
-
-		Error_handler();
-	}
-
-	__HAL_RCC_HSI_DISABLE(); // Turn off HSI because we are using HSE
-
-	/*
-	 * With the HSI, systick timer was wroking with 16MHX, now we are using
-	 * the HSE, so we need to reconfigure the ticks
-	 */
-	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
-	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-	UART2_Init();
-
-	memset(msg, 0, sizeof(msg));
-	sprintf(msg, "SYSCLOCK : %ldHz\r\n", HAL_RCC_GetSysClockFreq());
-	HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-	/*
-	memset(msg, 0, sizeof(msg));
-	sprintf(msg, "HLCK : %ldHz\r\n", HAL_RCC_GetHCLKFreq());
-	HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-
-	memset(msg, 0, sizeof(msg));
-	sprintf(msg, "PCLK1 : %ldHz\r\n", HAL_RCC_GetPCLK1Freq());
-	HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-
-	memset(msg, 0, sizeof(msg));
-	sprintf(msg, "PCLK2 : %ldHz\r\n", HAL_RCC_GetPCLK2Freq());
-	HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-	*/
 
 	while(1);
 
 	return 0;
 }
 
+
+void SystemClockConfig(uint8_t clock_freq){
+
+
+	RCC_OscInitTypeDef osc_init;
+	RCC_ClkInitTypeDef clk_init;
+
+	osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	osc_init.HSIState = RCC_HSI_ON;
+	osc_init.HSICalibrationValue = 16;
+	osc_init.PLL.PLLState = RCC_PLL_ON;
+	osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+
+	switch(clock_freq){
+
+		case SYS_CLOCK_FREQ_50MHZ:
+			osc_init.PLL.PLLM = 16;
+			osc_init.PLL.PLLN = 100;
+			osc_init.PLL.PLLP = 2;
+			osc_init.PLL.PLLQ = 2;
+			break;
+
+		case SYS_CLOCK_FREQ_84MHZ:
+			osc_init.PLL.PLLM = 16;
+			osc_init.PLL.PLLN = 168;
+			osc_init.PLL.PLLP = 2;
+			osc_init.PLL.PLLQ = 2;
+			break;
+
+		case SYS_CLOCK_FREQ_120MHZ:
+			osc_init.PLL.PLLM = 16;
+			osc_init.PLL.PLLN = 240;
+			osc_init.PLL.PLLP = 2;
+			osc_init.PLL.PLLQ = 2;
+			break;
+
+		default:
+			return;
+	}
+
+	if ( HAL_RCC_OscConfig(&osc_init) != HAL_OK){
+		Error_handler();
+	}
+}
 
 /*
  * Function to initialize the UART2 peripheral
