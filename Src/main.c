cff028f1dff99474ae458aeb69f2176795f989d8
juanpg9656@gmail.com <juanpg9656@gmail.com>
2021-01-24 11:52:40 -0500

    Tried test but not working fine in tera term. I believe oscillator and clock are configured fine, but there's a problem with the USART2 configuration

--- Src/main.c
+++ Src/main.c
@@ -8,6 +8,7 @@
 #include "stm32f4xx.h"
 #include "main.h"
 #include <string.h>
+#include <stdint.h>
 
 /*
  * Functions prototypes
@@ -27,6 +28,7 @@ int main(){
 
 	RCC_OscInitTypeDef osc_init;
 	RCC_ClkInitTypeDef clk_init;
+	char msg[100];
 
 	/* Basic initialization */
 	HAL_Init();
@@ -35,7 +37,8 @@ int main(){
 
 	/*
 	 * Oscillator initialization. We are not actually using HSE because it does not exist
-	 * But, we use the bypass mode and is configured in this way
+	 * But, we use the bypass mode and is configured in this way. This initialization
+	 * starts the HSE, but that doesn't mean the system clock will use it
 	 */
 	memset(&osc_init, 0, sizeof(osc_init)); //Maybe there is garbage values, so memset to 0
 	osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSE;
@@ -45,6 +48,48 @@ int main(){
 		Error_handler();
 	}
 
+	/*
+	 * This part configures the clock to be used as the system clock and
+	 * set different preescalers
+	 */
+	clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+						 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+	clk_init.AHBCLKDivider = RCC_SYSCLK_DIV2;
+	clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+	clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+	//This API changes from HSI to HSE
+	if(HAL_RCC_ClockConfig(&clk_init, FLASH_ACR_LATENCY_0WS) != HAL_OK){
+
+		Error_handler();
+	}
+
+	__HAL_RCC_HSI_DISABLE(); // Turn off HSI because we are using HSE
+
+	/*
+	 * With the HSI, systick timer was wroking with 16MHX, now we are using
+	 * the HSE, so we need to reconfigure the ticks
+	 */
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+	UART2_Init();
+
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "SYSCLOCK : %ldHz\r\n", HAL_RCC_GetSysClockFreq());
+	HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+	/*
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "HLCK : %ldHz\r\n", HAL_RCC_GetHCLKFreq());
+	HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "PCLK1 : %ldHz\r\n", HAL_RCC_GetPCLK1Freq());
+	HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+
+	memset(msg, 0, sizeof(msg));
+	sprintf(msg, "PCLK2 : %ldHz\r\n", HAL_RCC_GetPCLK2Freq());
+	HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+	*/
 
 	while(1);
 
